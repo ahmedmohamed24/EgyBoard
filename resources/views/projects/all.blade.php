7b618ab0d72c205bfa3b47d35a7afa7f023dcc07
ahmedmohamed24 <ahmed@ahmed.localdomain>
2021-01-27 20:30:24 +0200

    [add-feature] project page

--- resources/views/projects/all.blade.php
+++ resources/views/projects/all.blade.php
@@ -3,18 +3,33 @@
     Projects 
 @endsection
 @section('content')
-    <h1 class="text-info">All Projects <a href="{{ route('project.create') }}" class="btn btn-info">Create</a></h1>
+    <div class="d-flex justify-content-between align-items-center w-100 mb-3">
+        <h4 class="text-muted">My Projects</h4>
+        <a  href="{{ route('project.create') }}" class="btn btn-info text-light">New Project</a>
+    </div>
     @if($projects->isEmpty())
         <p class="text-center fs-1 my-4 text-danger" style="font-size: 2rem">There is no PROJECTS yet</p>
     @else
-    <div class="row border-box">
+    
+    <div class="row">
         @foreach ($projects as $project )
-        <div class="card text-dark bg-light col-md-4 py-2 px-1 " style="">
-            <div class="px-2  border-box" style="font-size: 1.2rem; border-left:5px solid #6cb2eb;"><a href="{{ $project->path() }}">{{ $project->title }}</a></div>
-            <div class="card-body border-box">
-                <p class="card-text text-muted">{{ $project->description }}</p>
-            </div>
-        </div> 
+
+        <div class="col-md-4  mb-2">
+            <div class="shadow bg-white py-4 bd-callout bd-callout-info pl-2 rounded" style="border-left:6px solid #5bc0de;min-height: 100%;">
+                <h4 id="dealing-with-specificity" class=""><a href="{{ $project->path() }}" class="text-decoration-none text-dark">{{ Str::limit($project->title,25, '...')  }}</a></h4>
+                <p class="text-muted">
+                    {{ Str::limit($project->description, 250, '...') }}
+                </p>
+            </div> 
+        </div>
+        {{-- <div class="col-md-4">
+            <div class="card  shadow bg-white py-3 pr-1 mb-2 ml-1" style="">
+                <div class="px-2 py-2 " style="font-size: 1.2rem; border-left:6px solid #91c5f0;"><a href="{{ $project->path() }}" class="text-decoration-none text-black">{{ Str::limit($project->title,25, '...')  }}</a></div>
+                <div class="card-body ">
+                    <p class="card-text text-muted">{{ Str::limit($project->description, 250, '...') }}</p>
+                </div>
+            </div> 
+        </div> --}}
         @endforeach
     </div>
     <div class="mt-4 d-flex justify-content-center align-items-center">{{ $projects->links() }}</div>
--- resources/views/projects/project.blade.php
+++ resources/views/projects/project.blade.php
@@ -3,6 +3,49 @@
    {{ Str::limit($project->title, 15, '...') }} 
 @endsection
 @section('content')
-    <h1  class="text-info">{{ $project->title }}</h1> 
-    <p>{{ $project->description}}</p> 
+    <header class="d-flex justify-content-between align-items-center py-2">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-white">
+                <li class="breadcrumb-item"><a href="{{ route('project.all') }}">my projects</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{ Str::limit($project->title, 15, '...') }}</li>
+            </ol>
+        </nav>
+        <button class="btn btn-info text-light">New Project</button>
+    </header>
+    <main class="row ">
+        <div class="col-md-8">
+            <div class="tasks-container">
+                <h3 class="text-muted">Tasks</h3>
+                <div class="card shadow border-right-0 border-top-0 border-bottom-0 mb-2 border-primary">
+                    <div class="card-body">
+                       {{ $project->description }} 
+                    </div>
+                </div>
+                <div class="card shadow border-right-0 border-top-0 border-bottom-0 mb-2 border-primary">
+                    <div class="card-body">
+                       {{ $project->description }} 
+                    </div>
+                </div>
+                <div class="card shadow border-right-0 border-top-0 border-bottom-0 mb-2 border-primary">
+                    <div class="card-body">
+                       {{ $project->description }} 
+                    </div>
+                </div>
+                <h4 class="text-muted mt-4 mb-3">General Notes</h4>
+                <textarea name="notes" cols="30" rows="3" class="form-control"> {{ $project->description }}</textarea>
+
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class=" project-desc-container mb-2">
+                <div class="shadow bg-white py-4 bd-callout bd-callout-info pl-2 rounded" style="border-left:6px solid #5bc0de;min-height: 100%;">
+                    <h4 id="dealing-with-specificity" class=""><a href="{{ $project->path() }}" class="text-decoration-none text-dark">{{$project->title }}</a></h4>
+                    <p class="text-muted">
+                        {{ $project->description }}
+                    </p>
+                </div> 
+            </div>
+        </div>
+    </main>
 @endsection
